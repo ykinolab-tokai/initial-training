095010cc0b0b235de7dc3202bf5f4d89e49a6b10
重水健吾 <lgmizukg@gmail.com>
2024-10-30 16:40:50 +0900

    your message

---
+++ kshigemizu/chapter02/q02.py
@@ -0,0 +1,28 @@
+a = 1.2 + 3.8 
+print(type(a))
+
+a = 10 // 100
+print(type(a))
+
+a = 1>=0
+print(type(a))
+
+a = 'Hello World' == 'Hello World'
+print(type(a))
+
+a = not 'Chainer' == 'Hello World'
+print(type(a))
+
+a = all([True, True, False])
+print(type(a))
+
+a = any([True, True, False])
+print(type(a))
+
+a = abs(-3)
+print(type(a))
+
+a = 2 // 0
+print(type(a))
+
+
